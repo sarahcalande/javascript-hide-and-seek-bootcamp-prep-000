d838416fbf11f5ca261efd465bfbb7fd35cf52dd
scalande@yahoo.com <scalande@yahoo.com>
2018-06-21 13:57:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,12 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
+    let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
   
 }
